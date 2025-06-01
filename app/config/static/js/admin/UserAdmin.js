b761bc2695ff4b00cb852a2994dcfcccd6494117
JoelAlcendra <joelalcendra11@gmail.com>
2025-06-01 18:53:01 -0500

    Pulidora

--- app/config/static/js/admin/UserAdmin.js
+++ app/config/static/js/admin/UserAdmin.js
@@ -1,14 +1,12 @@
+
+const sidebar = document.getElementById('sidebar');
+const content = document.getElementById('content');
+const body = document.body;
+const isMobile = window.innerWidth < 768;
+
 document.getElementById('sidebarCollapse').addEventListener('click', () => {
     sidebar.classList.toggle('active');
     content.classList.toggle('active');
-
-    // Ocultar el contenido de la barra de navegación cuando el sidebar esté cerrado
-    const navbar = document.querySelector('.navbar');
-    if (sidebar.classList.contains('active')) {
-        navbar.style.display = 'block';
-    } else {
-        navbar.style.display = 'none';
-    }
 
     // En móviles, añadir clase al body para oscurecer el fondo cuando el sidebar está abierto
     if (isMobile) {
@@ -20,23 +18,26 @@ document.getElementById('sidebarCollapse').addEventListener('click', () => {
     }
 });
 
-// Cerrar sidebar al hacer clic fuera de él en dispositivos móviles
 document.addEventListener('click', (e) => {
     if (isMobile &&
         sidebar.classList.contains('active') &&
         !sidebar.contains(e.target) &&
         !document.getElementById('sidebarCollapse').contains(e.target)) {
         sidebar.classList.remove('active');
+        content.classList.remove('active');
         body.classList.remove('sidebar-active');
     }
 });
+
 
 // Ajustar el diseño cuando cambia el tamaño de la ventana
 window.addEventListener('resize', () => {
     const currentIsMobile = window.innerWidth < 768;
 
-    // Si cambia de móvil a desktop o viceversa
-    if (currentIsMobile !== isMobile) {
-        location.reload(); // Recargar para aplicar los estilos correctos
+
+    if (!currentIsMobile && isMobile) {
+        sidebar.classList.remove('active');
+        content.classList.remove('active');
+        body.classList.remove('sidebar-active');
     }
 });
